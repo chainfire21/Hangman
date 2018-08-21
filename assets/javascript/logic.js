b6f37ec7683dcd38c7e2715baa53664cfcfeca51
unknown <paredfed@gmail.com>
2018-08-20 20:42:09 -0500

    fixed reset error

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,40 +1,37 @@
 var wordArray = ["music", "gaming", "soccer", "tennis", "origami"];
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var guessArray;
+var guessArray = [];
 var randNum;
 var currentWord;
 var winCount = 0;
 var lossCount = 0;
 
-newRun();
+initialSetup();
 
-function newRun() {
-    initialSetup();
-    document.addEventListener("keypress", function () {
-        var keyName = event.key;
-        console.log(currentWord);
-        var correctGuess = false;
-        for (i = 0; i < alphabet.length; i++) {
-            if (keyName === alphabet[i]) {
-                for (j = 0; j < currentWord.length; j++) {
-                    if (keyName === currentWord.charAt(j)) {
-                        correctGuess = true;
-                        guessArray[j] = keyName;
-                        console.log(guessArray);
-                        updateGuess();
-                    }
+document.addEventListener("keypress", function () {
+    var keyName = event.key;
+    console.log(currentWord);
+    for (i = 0; i < alphabet.length; i++) {
+        if (keyName === alphabet[i]) {
+            for (j = 0; j < currentWord.length; j++) {
+                if (keyName === currentWord.charAt(j)) {
+                    guessArray[j] = keyName;
+                    console.log(guessArray);
+                    updateGuess();
                 }
             }
         }
-        checkWin();
-    });
-}
+    }
+    checkWin();
+});
+
+
 
 function initialSetup() {
-    guessArray=[];
+    guessArray = ["_ "];
     randNum = Math.floor(Math.random() * wordArray.length);
     currentWord = wordArray[randNum];
-    for (i = 0; i < currentWord.length; i++) {
+    for (i = 0; i < currentWord.length - 1; i++) {
         guessArray.push("_ ");
     }
     document.getElementById("blankSpaces").innerHTML = guessArray.join("");
@@ -59,6 +56,6 @@ function checkWin() {
     if (isGuessed) {
         winCount++;
         document.getElementById("wins").innerHTML = winCount;
-        newRun();
+        initialSetup();
     }
 }
