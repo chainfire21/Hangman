7cbeb764d767190f8b0f2c411730c42ab822bed2
unknown <paredfed@gmail.com>
2018-08-21 19:29:02 -0500

    with lives

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,27 +1,30 @@
 var wordArray = ["music", "gaming", "soccer", "tennis", "origami"];
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessArray = [];
+var correct;
 var randNum;
 var currentWord;
 var winCount = 0;
 var lossCount = 0;
+var lives = 5;
 
 initialSetup();
 
 document.addEventListener("keypress", function () {
     var keyName = event.key;
+    correct = false;
     console.log(currentWord);
     for (i = 0; i < alphabet.length; i++) {
         if (keyName === alphabet[i]) {
             for (j = 0; j < currentWord.length; j++) {
                 if (keyName === currentWord.charAt(j)) {
                     guessArray[j] = keyName;
-                    console.log(guessArray);
-                    updateGuess();
+                    correct = true;
                 }
             }
         }
     }
+    updateGuess();
     checkWin();
 });
 
@@ -29,6 +32,8 @@ document.addEventListener("keypress", function () {
 
 function initialSetup() {
     guessArray = ["_ "];
+    lives=5;
+    document.getElementById("lives").innerHTML = lives;
     randNum = Math.floor(Math.random() * wordArray.length);
     currentWord = wordArray[randNum];
     for (i = 0; i < currentWord.length - 1; i++) {
@@ -43,19 +48,24 @@ function updateGuess() {
 
 function checkWin() {
     var isGuessed = true;
-    console.log("BEFORE LOOP:");
-    console.log(isGuessed);
+    console.log(lives);
+    if (!correct) {
+        lives--;
+    }
+    document.getElementById("lives").innerHTML = lives;
     for (i = 0; i < guessArray.length; i++) {
         if (guessArray[i] === "_ ") {
             isGuessed = false;
-            console.log("Current char: " + guessArray[i]);
         }
     }
-    console.log("AFTER LOOP:");
-    console.log(isGuessed);
     if (isGuessed) {
         winCount++;
         document.getElementById("wins").innerHTML = winCount;
+        initialSetup();
+    }
+    if (lives === 0) {
+        lossCount++;
+        document.getElementById("losses").innerHTML = lossCount;
         initialSetup();
     }
 }
